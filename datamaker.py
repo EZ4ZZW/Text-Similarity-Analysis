bb9ea53a15f682fff3ddec56ae1be3038a1db788
EZ4ZZW <zhiwayzhang@outlook.com>
2021-05-03 18:56:18 +0800

    add: data maker

--- datamaker.py
+++ datamaker.py
@@ -12,11 +12,11 @@ text = data['text']
 
 test_id = [i for i in range(0, len(text))]
 cnt = 0
-
+tt = 6973
 for i in text:
-    description_x.append(text[2333])
+    description_x.append(text[tt])
     description_y.append(i)
-print(len(description_x))
-print(len(description_y))
+
 outfile = pd.DataFrame({"test_id": test_id,"description_x": description_x, "description_y": description_y})
-outfile.to_csv("badData.csv", index = False)
+
+outfile.to_csv("singleData.csv", index = False)
